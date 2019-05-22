433e9930f940fcb5c26ad5d9238f02ad9d1c1175
=DamjanRadev <radevdamjan@gmail.com>
2019-05-22 12:05:47 +0200

    Initial setup for auth(sessions and cookies)

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require("body-parser");
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
 //ROUTES
 const questionRoutes = require("./routes/question");
-const indexPage = require('./routes/index');
+const indexPage = require("./routes/index");
+const authRoutes = require("./routes/auth");
 
 //MODELS
-const User = require('./models/user');
-
-
+const User = require("./models/user");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(async(req, res, next) => {
+app.use(async (req, res, next) => {
   const user = await User.findOne();
   req.user = user;
   next();
-})
+});
 
 app.use("/questions", questionRoutes);
 app.use(indexPage);
+app.use(authRoutes);
 
 mongoose
   .connect("mongodb://localhost/questionary", { useNewUrlParser: true })
@@ -36,12 +35,12 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: "Damjan",
+          name: "Damjan"
         });
         user.save();
       }
     });
-    
+
     app.listen(3000);
   })
   .catch(err => console.log(err));
